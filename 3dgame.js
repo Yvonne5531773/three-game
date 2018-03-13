0e46d85b546fb4eedd5c26ee10e317bdaa0312d7
lizihao@cmcm.com <5201314zy>
2018-03-13 16:36:14 +0800

    update

--- 3dgame.js
+++ 3dgame.js
@@ -9,21 +9,28 @@ let renderer,
 	cube = [];  //snake body object
 let plane, 
 	fo,
-	nx = 40, ny = 40, size = 20, start_point_x = 100, start_point_y = 50,
+	nx = 40, //范围宽
+	ny = 40, //范围高
+	size = 20,
+	start_point_x = 100,
+	start_point_y = 50,
 	len = 3,
 	head_pos_x = 10,
 	head_pos_y = 10,
 	head_for = 2,
 	dir_x = [0, -1, 1, 0],
 	dir_y = [1, 0, 0, -1],
-	status = -1;//the status of the game, -1 represents not start
+	status = -1;  //the status of the game, -1 represents not start
 let pauseFlag = true;  //in order to support pause function
 let board = []  //The state of game
 let the_last_head = head_for;  //The direction of snake
 let snake = [] //store snake
-let clickCount = 0
+let clickCount = 0,
+	gameover = false
 
 export default class game3d {
+	aRequest = {}
+
 	constructor() {
 		this.init()
 	}
@@ -34,7 +41,7 @@ export default class game3d {
 			canvas: canvas,
 			antialias: true
 		});
-		renderer.setSize(960, 600);
+		renderer.setSize(width, height);
 		renderer.shadowMapEnabled = true;
 		renderer.setClearColor('pink', 1.0);
 
@@ -118,22 +125,23 @@ export default class game3d {
 			cube[i].position.x = snake[i].x * 10 - 200;
 			cube[i].position.y = -snake[i].y * 10 + 190;
 		}
-		camera.position.y = -snake[0].y * 3 - 300;
 		camera.position.x = snake[0].x * 3 - 100;
+		camera.position.y = -snake[0].y * 3 - 300;
 		renderer.render(scene, camera);
 	}
 
 	getMove() {
-		// console.log('in move')
 		let tx = snake[0].x + dir_x[head_for];
 		let ty = snake[0].y + dir_y[head_for];
 		//tx = (tx + nx) % nx;
 		//ty = (ty + ny) % ny;
+		console.log('in move tx', tx)
+		console.log('in move ty', ty)
 		if (tx >= 0 && tx < nx && ty >= 0 && ty < ny) {
-			if (board[tx][ty] != 1) {
+			if (board[tx][ty] !== 1) {
 				the_last_head = head_for;
-				if (board[tx][ty] == 2) {
-					snake[len] = new Object();
+				if (board[tx][ty] === 2) {
+					snake[len] = {}
 					snake[len].x = snake[len - 1].x;
 					snake[len].y = snake[len - 1].y;
 					cube[len] = this.createCube(10, 10, 10);
@@ -153,18 +161,15 @@ export default class game3d {
 				snake[0].y = ty;
 			}
 			else {
-				if (the_last_head + head_for != 3) {
-					wx.showToast({title: "game over!\ryour score is " + len})
-					location.reload();
+				if (the_last_head + head_for !== 3) {
+					this.gameover()
 				}
 				else {
 					head_for = the_last_head;
 				}
 			}
-		}
-		else {
-			wx.showToast({title: "game over!\ryour score is " + len})
-			location.reload();
+		} else {
+			this.gameover()
 		}
 		for (let i = 0; i < nx; i++) {
 			for (let k = 0; k < ny; k++) {
@@ -175,6 +180,8 @@ export default class game3d {
 		for (let i = 0; i < len; i++) {
 			board[snake[i].x][snake[i].y] = 1;
 		}
+		// console.log('in move snake', snake)
+		// console.log('in move cube', cube)
 	}
 
 	getFood() {
@@ -194,7 +201,8 @@ export default class game3d {
 			this.getMove()
 		}
 		this.render();
-		window.requestAnimationFrame(this.run.bind(this), canvas);
+		this.aRequest = window.requestAnimationFrame(this.run.bind(this), canvas);
+		gameover && window.cancelAnimationFrame(this.aRequest);
 	}
 
 	onTouchStart(event) {
@@ -213,6 +221,13 @@ export default class game3d {
 		// 	head_for = 2;
 		// if (keynum == 80)
 		// 	pauseFlag = !pauseFlag;
+	}
+
+	gameover() {
+		console.log('gameover request', this.aRequest)
+		gameover = true
+		wx.showToast({title: "game over!\ryour score is " + len})
+		// location.reload();
 	}
 	
 	// start() {
